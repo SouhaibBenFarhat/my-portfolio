310a7a96d536290cb51725fda64cca4abb33e20a
souhaibbenfarhat <souhaib.benfarhat@esprit.tn>
2019-07-07 11:08:03 +0100

    project files

---
+++ src/Static/Data.js
@@ -0,0 +1,41 @@
+export const projects = [
+    {
+        name: '',
+        tag: '',
+        shortDescription: '',
+        thumbnail: '',
+        longDescription: '',
+        links: [{name: '', url: ''}],
+        screenshots: ['', '', '', '', '', ''],
+        parts: [
+            {
+                name: '',
+                description: '',
+                githubLink: '',
+                tags: ['', '', '', '', '']
+            },
+            {
+                name: '',
+                description: '',
+                githubLink: '',
+                tags: ['', '', '', '', '']
+            }
+        ],
+        features: [
+            {title: '', description: ''}
+        ]
+
+    }
+];
+
+const languages = [
+    {language: '', level: ''},
+    {language: '', level: ''},
+    {language: '', level: ''}
+]
+
+const awards = [
+    {title: '', date: '', link: ''},
+    {title: '', date: '', link: ''},
+    {title: '', date: '', link: ''}
+]
